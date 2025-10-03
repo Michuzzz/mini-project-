951c39184f04e7ae6e223e67de7a73b76de00186
Michuzzz <michellepaulkoottala@gmail.com>
2025-10-03 18:51:10 +0530

    Delete foundation-matcher/build.sh

--- foundation-matcher/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Install system dependencies for OpenCV and MediaPipe
-apt-get update
-apt-get install -y libgl1-mesa-glx libglib2.0-0 libsm6 libxext6 libxrender-dev libgomp1 libgtk-3-dev
