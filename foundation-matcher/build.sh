a666d7af4c7f4cc2e9b809f30f06ace99a2ada37
Michuzzz <michellepaulkoottala@gmail.com>
2025-10-03 15:06:25 +0530

    Create build.sh

---
+++ foundation-matcher/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Install system dependencies for OpenCV and MediaPipe
+apt-get update
+apt-get install -y libgl1-mesa-glx libglib2.0-0 libsm6 libxext6 libxrender-dev libgomp1 libgtk-3-dev
